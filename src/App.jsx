9eb5f167f33bbdcae8995daa6baa4a478c6cbe6e
Brandt322 <bgamboaa98@gmail.com>
2024-02-02 00:38:33 -0500

    feat: :sparkles: changed the css library to Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Button from "@mui/material/Button";
+
 function App() {
   return (
-    <>
+    <div>
       <h1>New meta</h1>
-    </>
+      <Button variant="contained" color="success">
+        Hello world
+      </Button>
+    </div>
   );
 }
 
